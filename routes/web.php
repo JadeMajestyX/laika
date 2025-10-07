c68dd2ddf6eff103b4a9f7b8b473de82bb5815c6
Sandra0602 <srodriguez46@ucol.mx>
2025-10-06 22:06:42 -0600

    Actualización de rutas en web.php

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,25 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('dashboard');
+    })->name('dashboard');
+
+Route::get('/mascotas', function () {
+    return view('mascotas');
+    })->name('mascotas');
+
+Route::get('/inventario', function () {
+    return view('inventario');
+    })->name('inventario');
+
+Route::get('/trabajadores', function () {
+    return view('trabajadores');
+    })->name('trabajadores');
+
+Route::get('/reportes', function () {
+    return view('reportes');
+    })->name('reportes');
+
+Route::get('/configuracion', function () {
+    return view('configuracion');
+    })->name('configuracion');
