0b00749d0bbf4415941d15de4254ff4c8251b028
Jose Angel Alvarez Carranza <joseangelalvarezx@gmail.com>
2025-10-08 09:07:07 -0600

    feat: Actualizar controlador de dashboard para incluir información del usuario autenticado y mostrar nuevos clientes del mes

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -13,6 +13,15 @@ class DashboardController extends Controller
 
         $data = User::all()->where('created_at', '>=', $ultimoMes)->count();
 
+        //info del usuario autenticado
+        $user = auth()->user();
+
+
+        //juntar info del usuario con la data
+        $data = ['user' => $user,
+            'new_users_last_month' => $data,
+        ];
+
         return view('dashboard', compact('data'));
     }
 }
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -62,7 +62,7 @@
         <div class="col-md-3 mb-3">
           <div class="card shadow-sm text-center">
             <div class="card-body">
-              <h4>{{$data}}</h4>
+              <h4>{{$data['new_users_last_month']}}</h4>
               <p class="mb-0">Clientes nuevos (Mes)</p>
             </div>
           </div>
