406477982337fbba258e3aa07ac7186dcf4dc269
Jose Angel Alvarez Carranza <joseangelalvarezx@gmail.com>
2025-10-13 10:14:47 -0600

    Agregar controlador y vista para gestionar citas

--- app/Http/Controllers/CitaController.php
+++ app/Http/Controllers/CitaController.php
@@ -3,8 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Cita;
 
 class CitaController extends Controller
 {
-    
+    public function index(){
+
+$citas = Cita::with(['clinica', 'servicio', 'mascota', 'creador'])
+             ->orderBy('fecha', 'desc')
+             ->paginate(10);
+
+                     $usuario = auth()->user();
+
+        return view('citas', compact('citas', 'usuario'));
+    }
 }
