415d84915cd966d510fc48ba3a523c05ace70dd3
Jose Angel Alvarez Carranza <jalvarez77@ucol.mx>
2025-11-03 17:25:55 -0600

    Refactor code structure for improved readability and maintainability

--- app/Http/Requests/ProfileUpdateRequest.php
+++ app/Http/Requests/ProfileUpdateRequest.php
@@ -16,7 +16,9 @@ class ProfileUpdateRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'name' => ['required', 'string', 'max:255'],
+            'nombre' => ['required', 'string', 'max:255'],
+            'apellido_paterno' => ['required', 'string', 'max:100'],
+            'apellido_materno' => ['nullable', 'string', 'max:100'],
             'email' => [
                 'required',
                 'string',
