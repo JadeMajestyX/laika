6ff880d412537dc6590b028000b313391b48bf35
Jose Angel Alvarez Carranza <jalvarez77@ucol.mx>
2025-11-03 18:32:26 -0600

    refactor: reemplazar el logo de la aplicación por un ícono y texto en la navegación

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -6,7 +6,7 @@
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
                     <a href="{{ route('dashboard') }}">
-                        <x-application-logo class="block h-9 w-auto fill-current text-gray-800 dark:text-gray-200" />
+                        <i class="bi bi-heart-pulse-fill me-2"></i> Laika
                     </a>
                 </div>
 
