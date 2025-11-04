a758b5fe147b2045b21a0faeb1345eebcc749478
Jose Angel Alvarez Carranza <jalvarez77@ucol.mx>
2025-11-03 18:29:51 -0600

    refactor: actualizar enlaces de navegación para redirigir a la página de inicio

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -11,13 +11,12 @@
                 </div> --}}
 
                 <!-- Navigation Links -->
-                {{-- <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                        {{ __('Dashboard') }}
+                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                    <x-nav-link :href="route('welcome')" :active="request()->routeIs('welcome')">
+                        {{ __('Inicio') }}
                     </x-nav-link>
                 </div>
-            </div> --}}
-            
+            </div>
 
             <!-- Settings Dropdown -->
             <div class="hidden sm:flex sm:items-center sm:ms-6">
