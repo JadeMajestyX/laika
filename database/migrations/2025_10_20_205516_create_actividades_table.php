b2b525b547a1ffa6ae774e98cb0cafb7782a885a
Jose Angel Alvarez Carranza <jalvarez77@ucol.mx>
2025-11-15 22:46:03 -0600

    feat: Modificar comportamiento de eliminación en cascada para el campo user_id en la tabla actividades

--- database/migrations/2025_10_20_205516_create_actividades_table.php
+++ database/migrations/2025_10_20_205516_create_actividades_table.php
@@ -13,10 +13,19 @@ return new class extends Migration
     {
         Schema::create('actividades', function (Blueprint $table) {
             $table->id();
-            $table->foreignId("user_id")->constrained("users")->nullable()->onDelete("set null");
+            
+            // Al eliminar un usuario, user_id se volverá NULL
+            $table->foreignId("user_id")
+                  ->nullable()
+                  ->constrained("users")
+                  ->nullOnDelete();
+
             $table->string("accion");
             $table->string("modelo");
+            
+            // Si quieres que la FK modelo_id también tenga onDelete, agrega ->nullOnDelete()
             $table->foreignId("modelo_id")->nullable();
+            
             $table->text("detalles")->nullable();
             $table->ipAddress("ip_address")->nullable();
             $table->string("navegador")->nullable();
