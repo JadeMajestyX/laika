c8ad4d17476e195392ee35c39b09593c366c44e8
Jose Angel Alvarez Carranza <joseangelalvarezx@gmail.com>
2025-11-25 10:16:30 -0600

    fix: corregir condición de redirección para el rol de veterinario

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -29,7 +29,7 @@ class AuthenticatedSessionController extends Controller
         $request->session()->regenerate();
 
         //si el rol del usuario es v redireccionar a la vista de veterinario
-        if (Auth::user()->role === 'V') {
+        if (Auth::user()->role === ':V') {
             return redirect()->intended(route('vet.dashboard', absolute: false));
         }
 
